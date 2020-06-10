2cbf5d1be7bd581f5d9937a9a352e5b1eaac95ce
P_7_Rathore <rathore@example.com>
2020-06-10 22:54:37 +0530

    create a function component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React, {Component} from 'react';
 // import logo from './logo.svg';
+import Person from './Person/Person.js'
 import './App.css';
 
 class App extends Component {
-  
   render(){
-    // return (
-    //   <div className="App">
-    //     <h1> Hii, I'm a React App</h1>
-    //   </div>
-    // );
-    // return (React.createElement('div', null, 'h1','hello')); //o/p: h1hello
-    // return (React.createElement('div', null, React.createElement("h1", null, 'hello'))); // o/p: hello
-    return (React.createElement('div',{className: 'App'}, React.createElement('h1', null, 'hello')))//o/p: it's print at center b/c class is put
+    return (
+      <div className="App">
+        <h1> Hii, I'm a React App</h1>
+        <Person /> 
+      </div>
+    );
   }  
-
 }
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,11 @@
+import React from 'react'
+// const person = function () {
+    //it's a es5 normal function 
+// }
+
+
+const person = () => {// it's a ES6 Arrow function 
+    return <p> I'm a Person! </p> 
+};
+
+export default person;
