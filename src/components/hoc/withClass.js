d7df153d976394b996f3f24d0268a12504d69118
P_7_Rathore <rathore@example.com>
2020-06-25 22:53:03 +0530

    Passing Unknown Props

--- src/components/hoc/withClass.js
+++ src/components/hoc/withClass.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
+
+// const withClass = (WrappedComponent, className) => {
+//     return (props) => ( // here we return function 
+//         <div className={className}>
+//             <WrappedComponent {...props}/>
+//         </div>    
+//     )
+// };
 
 const withClass = (WrappedComponent, className) => {
-    return (props) => (
-        <div className={className}>
-            <WrappedComponent />
-        </div>    
-    )
-    };
-
+    return class extends Component {    // Now Return class 
+        render () {
+            return (
+                <div className={className}>
+                    <WrappedComponent {...this.props} />
+                </div>
+            )
+        }
+    }
+}
 export default withClass;
